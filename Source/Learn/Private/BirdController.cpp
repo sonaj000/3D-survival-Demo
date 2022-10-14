fa5ef0fbdf5171fa58391067af57367d50d11087
sonaj000 <jasonxu20000505@gmail.com>
2022-10-13 17:46:31 -0700

    planning

--- Source/Learn/Private/BirdController.cpp
+++ Source/Learn/Private/BirdController.cpp
@@ -106,6 +106,30 @@ void ABirdController::Flocking()
 {
 	/* the flocking logic should be managed by a flock manager. we can just have the boxes either stack on top of each other */
 	//import a flockmanager. 
+	/*flock manager should keep track of all the birds on the level via get all actors of class function to store them all in a array
+	boolean is flocking is true. 
+	
+	
+	//logic for the flock manager who will handle the flocking unit. 
+	use oncomponentoverlap. so have a secondary sphere that's not hitbox and basically if there is an actor overlapping that is a bird, add it to a map so use umap on the flockmanager. 
+	there will be a group for each bird that is stored in the flock so unit from array index is key and value is group number. 
+
+	so first check for each actor in overlap that is a bird if there is an input in the map for the bird name. 
+	if (yes)
+	return
+	else:
+	proceed with making an array in the flockmanager and add the bird and all other bird actors overlapping into the array. add each bird to the map and their corresponding group. 
+
+
+	for now, let's just make an array for each group and add the bird in there. so basically we randomly select a bird from the array and then have all the birds in that group flock towards the llocation of that actor. So 
+	CHANGE THE FUNCTION ARGUMENT FROM VOID TO FVECTOR. 
+
+	flock manager should have one tarray that has all the birds, one map that gets filled for each group and bird in the group, and i lied we can store the array in the map. maybe to make it more balanced, set the flock speed slower?
+
+	so one thing we can do is replace all the birds with simply one bird with a bigger mesh and just name this big bird lol. would be easier and we would not have to worru about coding things like having all the birds stick together and stay togetehr which is like weird
+	so one thing is all these birds need to have destructors to destroy at the end. 
+	
+	*/
 }
 
 void ABirdController::StateChange(int statenum)
